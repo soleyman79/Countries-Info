2e660eac1fd0e9503b201dac4518a43f3019a0bf
Mohammad Hossein Soleymani <hssoleymanmh@gmail.com>
2024-05-20 09:04:35 +0330

    Change application properties format

--- src/main/java/edu/web/countries/CountriesApplication.java
+++ src/main/java/edu/web/countries/CountriesApplication.java
@@ -1,11 +1,9 @@
 package edu.web.countries;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@EnableAdminServer
 @SpringBootApplication
 public class CountriesApplication {
     public static void main(String[] args) {
